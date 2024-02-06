bfc9ceeaa8bc8f61c78fa6f4d29a3ebf5004f69e
Guilherme Costa <echina725@gmail.com>
2024-02-06 18:05:15 -0300

    feat: create poll model

--- polls/src/http/server.ts
+++ polls/src/http/server.ts
@@ -2,6 +2,10 @@ import fastify from "fastify";
 
 const app = fastify();
 
+app.post("/polls", () => {
+
+})
+
 app.listen({ port: 3333 })
     .then(() => {
         console.log("NLW http server is running!")
