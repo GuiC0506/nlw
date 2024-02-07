e6aa1162226a1935c1a1273c805ec8d37d675e6a
Guilherme Costa <echina725@gmail.com>
2024-02-06 23:07:33 -0300

    feat: add post and get polls endpoints

--- polls/src/http/routes/polls.ts
+++ polls/src/http/routes/polls.ts
@@ -5,15 +5,93 @@ import { z } from "zod";
 export async function createPoll(app: FastifyInstance) {
     app.post("/polls", async (req: FastifyRequest, reply:FastifyReply) => {
         const createPollBody = z.object({
-            title: z.string()
+            title: z.string(),
+            options: z.array(z.string())
         });
 
-        const { title } = createPollBody.parse(req.body);
+        const { title, options } = createPollBody.parse(req.body);
         const poll = await prisma.poll.create({
             data: {
-                title
+                title,
+                options: {createMany: {
+                    data: options.map(option => {
+                        return {
+                            title: option,
+                        }
+                    })
+                }}
             }
         });
+
+        /* prisma.pollOption.createMany({ */
+        /*     data: options.map(option => { */
+        /*         return { */
+        /*             title: option, */
+        /*             pollId: poll.id */
+        /*         } */
+        /*     }) */
+        /* }) */
+
+        return reply.status(201).send(poll);
+    })
+}
+
+export async function getPoll(app: FastifyInstance) {
+    app.get("/polls/:pollId", async(req, reply) => {
+        const getPollParams = z.object({
+            pollId: z.string().uuid()
+        });
+
+        const {pollId} = getPollParams.parse(req.params);
+
+        const poll = await prisma.poll.findUnique({
+            where: { id: pollId },
+            include: {options: {
+                select: {
+                    title: true
+                }
+            }}
+        })
+
+        return reply.status(200).send(poll);
+    })
+}
+
+
+export async function voteOnPoll(app: FastifyInstance) {
+    app.post("/polls/:pollId/votes", async (req: FastifyRequest, reply:FastifyReply) => {
+        const voteOnPollBody = z.object({
+            pollOptionId: z.string().uuid()
+        });
+
+        const voteOnPollParams = z.object({
+            pollId: z.string().uuid()
+        });
+
+        const { pollOptionId } = voteOnPollBody.parse(req.body);
+        const { pollId } = voteOnPollParams.parse(req.params);
+
+        const poll = await prisma.poll.create({
+            data: {
+                title,
+                options: {createMany: {
+                    data: options.map(option => {
+                        return {
+                            title: option,
+                        }
+                    })
+                }}
+            }
+        });
+
+        /* prisma.pollOption.createMany({ */
+        /*     data: options.map(option => { */
+        /*         return { */
+        /*             title: option, */
+        /*             pollId: poll.id */
+        /*         } */
+        /*     }) */
+        /* }) */
 
         return reply.status(201).send(poll);
     })
--- polls/src/http/server.ts
+++ polls/src/http/server.ts
@@ -1,9 +1,12 @@
 import fastify from "fastify";
-import { createPoll } from "./routes/polls";
+import { createPoll, getPoll, voteOnPoll } from "./routes/polls";
 
 const app = fastify();
 
 app.register(createPoll);
+app.register(getPoll);
+app.register(voteOnPoll);
+
 
 app.listen({ port: 3333 })
     .then(() => {
